57b860e7a1dcc9703260d7f2653b097801366f4d
Marko Prcac <marko.prcac@dinitcs.com>
2019-02-10 02:21:23 +0100

    Updates

---
+++ precache-manifest.6430c10c6dd2ce9834a9b615920afa10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61846de30c9e3e7da49c",
+    "url": "/demo-calculator/static/css/main.aafd780c.chunk.css"
+  },
+  {
+    "revision": "61846de30c9e3e7da49c",
+    "url": "/demo-calculator/static/js/main.61846de3.chunk.js"
+  },
+  {
+    "revision": "e6b449a3730e6482532c",
+    "url": "/demo-calculator/static/js/1.e6b449a3.chunk.js"
+  },
+  {
+    "revision": "d4b1377e2f4b30d1587f",
+    "url": "/demo-calculator/static/js/runtime~main.d4b1377e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a670e03f2c5cc0a77d84208a9409b25",
+    "url": "/demo-calculator/index.html"
+  }
+];
